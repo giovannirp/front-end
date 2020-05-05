9adda075a03bfb07e2c617bee847dd3adcaf8356
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2020-05-04 23:54:16 -0300

    adicionando arquivos javascript

---
+++ JAVASCRIPT/estrutura-decisao/programa2/js/aluno.js
@@ -0,0 +1,13 @@
+function situacao() {
+    var nota = document.getElementById("nota").value;
+
+    if(nota >= 6) {
+        document.getElementById("resposta").innerHTML = "Aluno aprovado";
+    } else {
+        document.getElementById("resposta").innerHTML = "Aluno Reprovado";
+    }
+}
+
+function limpar() {
+    document.getElementById("nota").value = "";
+}
