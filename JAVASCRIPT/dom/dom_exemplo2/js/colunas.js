65aee412d2a51cb8c068df51a68e28cb554f6e26
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2020-05-25 23:05:17 -0300

    adicionando exercicios de javascript

--- JAVASCRIPT/dom/dom_exemplo2/js/colunas.js
+++ JAVASCRIPT/dom/dom_exemplo2/js/colunas.js
@@ -5,3 +5,12 @@ function criarColuna() {
     novaColuna.appendChild(novoTexto);
     linha.appendChild(novaColuna);    
 }
+
+function outraTable() {
+    var table = document.getElementById("minhaTabela");
+    var linha = table.insertRow(0);
+    var coluna1 = linha.insertCell(0);
+    var coluna2 = linha.insertCell(1);
+    coluna1.innerHTML = "COLUNA 1";
+    coluna2.innerHTML = "COLUNA 2";
+}
---
+++ JAVASCRIPT/dom/dom_exemplo3/js/animate.js
@@ -0,0 +1,14 @@
+function movimenta() {
+    var elemento = document.getElementById("animacao");   
+    var posicao = 0;
+    var id = setInterval(quadro, 5);
+    function quadro() {
+      if (posicao == 350) {
+        clearInterval(id);
+      } else {
+        posicao++; 
+        elemento.style.top = posicao + "px"; 
+        elemento.style.left = posicao + "px"; 
+      }
+    }
+  }
